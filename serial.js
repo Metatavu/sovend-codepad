2aace0b9c50980dc0e3db666394b10b5d5852847
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-07-27 12:22:34 +0300

    Added serial listener

---
+++ serial.js
@@ -0,0 +1,61 @@
+/*jshint esversion: 6 */
+/* global __dirname */
+(() => {
+  'use strict';
+
+  const SerialPort = require('serialport');
+
+  class Serial {
+
+    constructor (device, baudRate, startByte, stopByte) {
+      this.startByte = startByte;
+      this.stopByte = stopByte;
+
+      this.port = new SerialPort(device, {
+        baudRate: baudRate
+      });
+
+      this.message = [];
+      this.onMessageListeners = [];
+
+      this.port.on('data', this._onData.bind(this));
+    }
+    
+    onMessage(callback) {
+      this.onMessageListeners.push(callback);
+    }
+
+    sendData(message) {
+      let data = new Buffer([this.startByte]);
+      data = Buffer.concat([data, Buffer.from(message, 'utf8')]);
+      data = Buffer.concat([data, new Buffer([this.stopByte])]);
+      this.port.write(data);
+    }
+
+    _receiveByte(byte) {
+      if (this.startByte === byte) {
+        this.message = [];
+      } else if (this.stopByte === byte) {
+        this._triggerMessageEvent();
+      } else {
+        this.message.push(byte);
+      }
+    }
+
+    _triggerMessageEvent() {
+      const messageString = new Buffer(this.message).toString('utf8');
+      for (let i = 0; i < this.onMessageListeners.length; i++) {
+        this.onMessageListeners[i](messageString);
+      }
+    }
+
+    _onData(data) {
+      for (let i = 0; i < data.length; i++) {
+        this._receiveByte(data[i]);
+      }
+    }    
+  }
+
+  module.exports = Serial;
+  
+})();
